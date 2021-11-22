6b3a423e1d724f1d4347e0818d5d1b145001060c
JJ Rohrer <jj.src@rohrer.org>
2021-11-22 09:48:03 -0500

    not sure - seemed to help

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ require_once(base_path('vendor/eleganttechnologies/grok/src/routes.php'));
 
          return view("{$asrGrok['bladePrefix']}::grok/index");
      })->name('grok');
-
-     Route::get('/grok/{vendorNameCamelCase}/{packageNameCamelCase}/{sub}', function ($vendorNameCamelCase, $packageNameCamelCase, $sub) {
-         $asrGrok = \ElegantTechnologies\Grok\GrokWrangler::getAsrGrok_byVendorPackage($vendorNameCamelCase, $packageNameCamelCase);
-         $viewPath = "{$asrGrok['bladePrefix']}::grok/$sub/index";
-
-         return view($viewPath);
-     })->name('grok');
+//
+//     Route::get('/grok/{vendorNameCamelCase}/{packageNameCamelCase}/{sub}', function ($vendorNameCamelCase, $packageNameCamelCase, $sub) {
+//         $asrGrok = \ElegantTechnologies\Grok\GrokWrangler::getAsrGrok_byVendorPackage($vendorNameCamelCase, $packageNameCamelCase);
+//         $viewPath = "{$asrGrok['bladePrefix']}::grok/$sub/index";
+//
+//         return view($viewPath);
+//     })->name('grok');
  }
